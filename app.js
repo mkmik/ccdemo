98f5ffb5ca21fc346b4d61741b4741d92dfe79d2
Marko Mikulicic <mmikulicic@gmail.com>
2026-01-14 17:04:42 +0100

    Done! I've added `utterance.lang = 'en-US'` to the speech synthesis configuration. This ensures the text-to-speech uses English (US) pronunciation consistently.

--- app.js
+++ app.js
@@ -271,6 +271,7 @@ class WeekdayGame {
         this.synthesis.cancel();
 
         const utterance = new SpeechSynthesisUtterance(text);
+        utterance.lang = 'en-US';
         utterance.rate = 0.9;
         utterance.pitch = 1;
         utterance.volume = 1;
